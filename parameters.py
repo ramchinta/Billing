6b94ab23deef86c9e4dad3eab080547b4af77885
Lakshman Chinta <mithrachinta@gmail.com>
2020-03-26 01:58:26 -0400

    Updated

---
+++ parameters.py
@@ -0,0 +1,8 @@
+import pymysql
+
+MYSQL_HOST = "database-1.cszampg2p55a.us-east-1.rds.amazonaws.com"
+MYSQL_USERNAME = "admin"
+MYSQL_PASSWORD = "Lakshman4"
+MYSQL_DATABASE = "ccpa"
+mysqlConn = pymysql.connect(host=MYSQL_HOST, port=3306, user=MYSQL_USERNAME, password=MYSQL_PASSWORD,
+                             database=MYSQL_DATABASE)
